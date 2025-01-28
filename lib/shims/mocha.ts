ef4b8deb55080423162e53b6decc96db33d397c1
Don Isaac <donald.isaac@gmail.com>
2025-01-28 12:16:58 -0800

    ci: add check workflow

--- lib/shims/mocha.ts
+++ lib/shims/mocha.ts
@@ -1,3 +1,7 @@
 import { beforeAll, afterAll } from "bun:test"
+declare global {
+    function before(fn: () => void | Promise<unknown>): void
+    function after(fn: () => void | Promise<unknown>): void
+}
 globalThis.before = beforeAll
 globalThis.after = afterAll
