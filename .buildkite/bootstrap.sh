9b69b14f1da87a5b9f0e64150d54881ab8954d1f
Don Isaac <donald.isaac@gmail.com>
2025-04-02 12:46:06 -0700

    bootstrap wip

--- .buildkite/bootstrap.sh
+++ .buildkite/bootstrap.sh
@@ -3,8 +3,14 @@
 set -eo pipefail
 
 curl -fsSL https://bun.sh/install | bash
+echo "bun installation dir:"
+ls -la ~/.bun
+
 echo "bashrc:"
 cat ~/.bashrc
-source ~/.bashrc
+. ~/.bashrc
+echo "PATH:"
+echo $PATH
+
 bun install
 bun src/index.ts
