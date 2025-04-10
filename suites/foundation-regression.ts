0d99696cc451d9c82857b412809766158b7f137a
Don Isaac <donald.isaac@gmail.com>
2025-04-10 16:41:46 -0700

    i am silly goose

--- suites/foundation-regression.ts
+++ suites/foundation-regression.ts
@@ -23,7 +23,7 @@ export default installAndTest('foundation regression', {
     },
     fastify: {
         repository: 'https://github.com/fastify/fastify.git',
-        test: steps.test.bun('express'),
+        test: steps.test.bun('fastify'),
         failing: true,
     },
     koa: {
